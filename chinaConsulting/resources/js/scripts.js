3eee9d0e0f4a86429636d29485f0a902f013768a
bomba65 <bkalzhan@gmail.com>
2018-02-24 17:11:41 +0600

    Add responsive nav

--- chinaConsulting/resources/js/scripts.js
+++ chinaConsulting/resources/js/scripts.js
@@ -2,6 +2,48 @@ $(document).ready(function(){
     $('.carousel').carousel({
       interval: 10000
     });
+    
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top -70
+            }, 1000)
+              return false;
+          }
+        }
+      });
+    
+    /* Mobile Nav */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--nav-body');
+        var icon = $('.js--nav-icon i')
+        
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    })
 });
 
 var hero
@@ -26,13 +68,12 @@ $(window).on('scroll', function () {
   });
 });
 
-nav.find('a').on('click', function () {
-  var $el = $(this)
-    , id = $el.attr('href');
-  
-  $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height
-  }, 1000);
-  
-  return false;
-});
+$(window).on("resize", function () {
+    if ($(window).width() > 767) {
+        $('.js--nav-body').css('display','');
+    } 
+    else {
+        $('.js--nav-icon i').addClass('ion-navicon-round');
+        $('.js--nav-icon i').removeClass('ion-close-round');
+    }
+}).resize();
