9afd7842b812f3e2960ad0878f995f040ae59279
bomba65 <bkalzhan@gmail.com>
2018-02-26 22:22:20 +0600

    Fix bugs, add close popup

--- chinaConsulting/resources/js/scripts.js
+++ chinaConsulting/resources/js/scripts.js
@@ -131,9 +131,17 @@ $(window).on("resize", function () {
 // Get the modal
 var modal = document.getElementById('mainModal');
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("js--close-popup")[0];
+
 // Get the button that opens the modal
 var btn2 = document.getElementById("showModal-2");
 var btn3 = document.getElementById("showModal-3");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
 // When the user clicks the button, open the modal 
 
