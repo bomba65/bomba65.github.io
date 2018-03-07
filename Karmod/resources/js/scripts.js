1fc97e1da20e1c8a1eb6b7e12fc4cca561eb57e4
bomba65 <bkalzhan@gmail.com>
2018-03-07 11:30:02 +0600

    Karmod version 1.0.0

---
+++ Karmod/resources/js/scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    //Gallery Popup
+    $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});    
+});
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("May 5, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("discount-counter").innerHTML = days + "<span>д</span>&nbsp;" + hours + "<span>ч</span>&nbsp;:&nbsp;"
+    + minutes + "<span>м</span>&nbsp;:&nbsp;" + seconds + "<span>с</span> ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
