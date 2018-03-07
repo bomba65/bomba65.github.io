ef61d6bc512e367732b8f7a4d5445e243f207b19
bomba65 <bkalzhan@gmail.com>
2018-03-07 12:32:40 +0600

    Karmod version 1.1.0

--- Karmod/resources/js/scripts.js
+++ Karmod/resources/js/scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    
+    $('.nav-body').waypoint(function(direction){
+        if (direction == "down") {
+            $('.nav-sticky').hide();
+            $( ".nav-sticky" ).slideDown( "slow", function() {
+                // Animation complete.
+              });
+        } else {
+            $('.nav-sticky').hide();
+        }
+    });
+    
     //Gallery Popup
     $('.popup-gallery').magnificPopup({
 		delegate: 'a',
@@ -16,7 +28,57 @@ $(document).ready(function(){
 				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
 			}
 		}
-	});    
+	});
+    
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+    .not('[href="#carouselExampleIndicators"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top -72
+            }, 1000)
+              return false;
+          }
+        }
+      });
+});
+
+var hero
+    , sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
 });
 
 
