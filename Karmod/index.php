22a6ad78474c0ffb63688e94e6e2ab4818e71ff0
bomba65 <bkalzhan@gmail.com>
2018-03-10 10:46:02 +0600

    Karmod add php

--- Karmod/index.php
+++ Karmod/index.php
@@ -1,33 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="This China Consulting site!">
-        
-        <link rel="stylesheet" type="text/css" href="./vendors/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="./vendors/css/normalize.css">
-        <link rel="stylesheet" type="text/css" href="./vendors/css/grid.css">
-        <link rel="stylesheet" type="text/css" href="./vendors/css/magnific-popup.css">
-        <link rel="stylesheet" type="text/css" href="./vendors/css/ionicons.min.css">
-        <link rel="stylesheet" type="text/css" href="./vendors/css/animate.css">
-        <link rel="stylesheet" type="text/css" href="./resources/css/style.css">
-        <link rel="stylesheet" type="text/css" href="./resources/css/queries.css">
-        <title>Blocks</title>
-        
-        <link rel="apple-touch-icon" sizes="120x120" href="./resources/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./resources/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./resources/favicons/favicon-16x16.png">
-        <link rel="manifest" href="./resources/favicons/site.webmanifest">
-        <link rel="mask-icon" href="./resources/favicons/safari-pinned-tab.svg" color="#5bbad5">
-        <link rel="shortcut icon" href="./resources/favicons/favicon.ico">
-        <meta name="msapplication-TileColor" content="#2d89ef">
-        <meta name="msapplication-config" content="./resources/favicons/browserconfig.xml">
-        <meta name="theme-color" content="#ffffff">
-    </head>
+    <?php include('./component/head.html');?>
     <body>
         <div class="main-container">
-            <?php include('./nav.html');?>
+            <?php include('./component/nav.html');?>
             <section id="section-main" class="section-main">
                 <div class="row">
                     <h1>Быстровозводмые сборные киоски и павильоны</h1>
---
+++ Karmod/pannel.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('./component/head.html');?>
+    <body>
+        <div class="main-container">
+            <?php include('./component/nav.html');?>
+            
+            <hr>
+            
+            <section class="section-technical-info">
+                <div class="row">
+                    <div class="technical-nav">
+                        <ul>
+                            <li><a href="#technical-data">Технические данные</a></li>
+                            <li><a href="#technical-floor">Основание / Пол</a></li>
+                            <li><a href="#technical-external-details">Внешние детали</a></li>
+                            <li><a href="#technical-roof">Кровля</a></li>
+                            <li><a href="#technical-additionally">Дополнительно</a></li>
+                        </ul>
+                    </div>
+                    <div class="technical-content">
+                        <p class="technical-info-header">
+                            Технические характеристики <a href="./cabin.html">кабин</a> / <span>панелей</span>
+                        </p>
+                        <img src="resources/img/tech.cabin.png" alt="">
+                        <div id="technical-data" class="technical-info">
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Сейсмостойкость
+                                </p>
+                                <p class="technical-info-text">
+                                    1-ая (высшая) степень степень интенсивности.
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Климатические условия
+                                </p>
+                                <p class="technical-info-text">
+                                    Третья климатическая зона (суровый климат)
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Снеговая нагрузка
+                                </p>
+                                <p class="technical-info-text">
+                                    80 кг/кв.м
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Ветровая нагрузка
+                                </p>
+                                <p class="technical-info-text">
+                                    80км/час (Кабина должна быть зафиксирована к поверхности)
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Металллические изделия
+                                </p>
+                                <p class="technical-info-text">
+                                    ST 37 Оцинкованная сталь
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Покрытие основания
+                                </p>
+                                <p class="technical-info-text">
+                                    TS EN 634-2 Цементно- стружечная плита - Огнестойкость класса “B”
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Изоляция
+                                </p>
+                                <p class="technical-info-text">
+                                    TS EN 13165 - Пенополиуретан -Огнестойкость класса “B”
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Несущая способность шасси
+                                </p>
+                                <p class="technical-info-text">
+                                    200 кг/м2
+                                </p>
+                            </div>
+                        </div>
+                        <div id="technical-floor" class="technical-info">
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Тип
+                                </p>
+                                <p class="technical-info-text">
+                                    Основание кабины имеет стальную конструкцию особой формы на основе цельных несущих профилей из нержавеющей стали.
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Покрытие пола
+                                </p>
+                                <p class="technical-info-text">
+                                    Огнеупорная цементно стружечная плита ЦСП без запаха толщиной 16 мм поверх которой настелен линолеум из ПВХ толщиной 2,5 мм.
+                                </p>
+                            </div>
+                        </div>
+                        <div id="technical-external-details" class="technical-info">
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Внешнее покрытие
+                                </p>
+                                <p class="technical-info-text">
+                                    Специальный изофталевый акриловый гелькоут из полиэстера, беловатого (флуоресцентного) цвета, который устойчив к агрессивной внешней среде и ультрафиолетовому излучению.
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Внутреннее покрытие
+                                </p>
+                                <p class="technical-info-text">
+                                    Специальный изофталевый акриловый гелькоут на основе полиэстера, беловатого (флуоресцентного) цвета, который устойчив к агрессивной внешней среде и ультрафиолетовому излучению.
+                                </p>
+                            </div>
+                        </div>
+                        <div id="technical-roof" class="technical-info">
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Устройство
+                                </p>
+                                <p class="technical-info-text">
+                                    Представляет собой сэндвич-панель из двух слоев стеклопластика с прослойкой из полиуретана. Кровельные панели особой формы соединяются специальными болтами со стенными панелями и заделываются антибактериальным силиконом, который изолирует от влаги, снега, пыли и бактерий, что обеспечивает санитарию и ухоженность.
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Кровельная изоляция
+                                </p>
+                                <p class="technical-info-text">
+                                    Прослойка полиуретана 42-ой плотности инъектируемая в панель под высоким давлением.
+                                </p>
+                            </div>
+                        </div>
+                        <div id="technical-additionally" class="technical-info">
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Устройство
+                                </p>
+                                <p class="technical-info-text">
+                                    Представляет собой сэндвич-панель из двух слоев стеклопластика с прослойкой из полиуретана. Кровельные панели особой формы соединяются специальными болтами со стенными панелями и заделываются антибактериальным силиконом, который изолирует от влаги, снега, пыли и бактерий, что обеспечивает санитарию и ухоженность.
+                                </p>
+                            </div>
+                            <div class="technical-info-sub">
+                                <p class="technical-info-name">
+                                    Кровельная изоляция
+                                </p>
+                                <p class="technical-info-text">
+                                    Прослойка полиуретана 42-ой плотности инъектируемая в панель под высоким давлением.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+                        
+            <div class="row"><hr></div>
+            
+            <footer>
+                <div class="row">
+                    <img class="logo-footer" src="./resources/img/logo_black.png" alt="Logo footer">
+                    <div class="social-icons">
+                        <img class="social-icon" src="./resources/icons/social.fb.svg" alt="Facebook">
+                        <img class="social-icon" src="./resources/icons/social.inst.svg" alt="Instagram">
+                        <img class="social-icon" src="./resources/icons/social.vk.svg" alt="VK">
+                    </div>
+                    <span>
+                        &copy; BlockStack. Все права защишены
+                    </span>
+                </div>
+            </footer>
+        </div>
+        
+        <div id="mainModal" class="popup">
+            <div class="popup-container">
+                <span class="js--close-popup">
+                    <i class="ion-close-round"></i>
+                </span>
+                <h2>Оставьте заявку</h2>
+                <p>и получите консультацию</p>
+                
+                <label>Ваше имя</label>
+                <input type="text" placeholder="Александра">
+                <label>Номер телефона</label>
+                <input type="number" placeholder="+7 (702) 502-62-85">
+                
+                <button class="btn btn-primary">Отправить заявку</button>
+            </div>
+        </div>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ></script>
+        <script src="vendors/js/bootstrap.min.js" ></script>
+        <script src="vendors/js/jquery.waypoints.min.js" ></script>
+        <script src="vendors/js/jquery.magnific-popup.min.js" ></script>
+        <script src="resources/js/scripts.js"></script>
+        
+    </body>
+    
+</html>
